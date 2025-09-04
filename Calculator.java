5f998e8792f96059c3f3d36c7092af040bd8e88a
Yerkebulan Torekhan <yerkebulan.torekhan@alumni.nu.edu.kz>
2025-09-04 11:13:01 +0500

    Day 2: Консольный калькулятор

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Выберите операцию (+, -, *, /): ");
+        char op = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b != 0) {
+                    result = a / b;
+                } else {
+                    System.out.println("Ошибка: деление на ноль!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Неизвестная операция");
+                return;
+        }
+
+        System.out.println("Результат: " + result);
+    }
+}
